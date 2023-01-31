0c3abf125db39f42d3695437dd29bf80729b48ba
happy-sudo <2271589536@qq.com>
2023-01-31 16:40:28 +0800

    字符串首字符大写

---
+++ stringspro/capitalize/capitalize.go
@@ -0,0 +1,20 @@
+package capitalize
+
+// Capitalize 字符首字母大写
+func Capitalize(str string) string {
+	var upperStr string
+	vv := []rune(str)
+	for i := 0; i < len(vv); i++ {
+		if i == 0 {
+			if vv[i] >= 97 && vv[i] <= 122 {
+				vv[i] -= 32
+				upperStr += string(vv[i])
+			} else {
+				return str
+			}
+		} else {
+			upperStr += string(vv[i])
+		}
+	}
+	return upperStr
+}
